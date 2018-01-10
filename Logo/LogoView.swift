90316f55dcad11ce4611d6176aadc4d110d64c62
PGMY <pgmyjp@gmail.com>
2018-01-10 18:28:24 +0900

    add project

---
+++ Logo/LogoView.swift
@@ -0,0 +1,21 @@
+//
+//  LogoView.swift
+//  Logo
+//
+//  Created by Mika Yamamoto on 2018/01/10.
+//  Copyright © 2018年 PGMY. All rights reserved.
+//
+
+import UIKit
+
+class LogoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
